43c6a4bea9e9aca817035e83a951ba60d40c4cbe
renezurbruegg <rene.zurbruegg@gmail.com>
2017-05-23 18:49:43 +0200

    Updated Alpha Beta Player

--- Reversi/src/reversiplayers/AlphaBetaMem.java
+++ Reversi/src/reversiplayers/AlphaBetaMem.java
@@ -1,167 +1,204 @@
 package reversiplayers;
 
 import reversi.*;
+import reversiplayers.ReversiUtils.GameStateNode;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class AlphaBetaMem implements ReversiPlayer{
+public class AlphaBetaMem implements ReversiPlayer {
 	private int color = 0;
 	private int other = 0;
-	
+
 	private long maxTime = 0;
 	private long tbuffer = 0;
 	private long t0 = 0;
 	private long tR = 0;
-	private int maxDepth = 50;
+	private int maxDepth = 20;
+	private List<Coordinates> allMoves = new ArrayList<Coordinates>(64);
 	
 	PointCalc PC;
-	
-	public void initialize(int color, long timeout){
+
+	public void initialize(int color, long timeout) {
 		this.color = color;
 		this.other = Utils.other(color);
 		this.maxTime = timeout - tbuffer;
+		
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 8; j++) {
+				allMoves.add(new Coordinates(i,j));
+			}
+		}
+		
 		if (color == GameBoard.RED)
-			System.out.println("AlphaBeta ist Spieler RED.");
+			System.out.println("AlphaBetaMem ist Spieler RED.");
 		else if (color == GameBoard.GREEN)
-			System.out.println("AlphaBeta ist Spieler GREEN.");
+			System.out.println("AlphaBetaMem ist Spieler GREEN.");
 		PC = new PointCalc();
 	}
-	
-	public Coordinates nextMove(GameBoard gb){
+
+	public Coordinates nextMove(GameBoard gb) {
 		t0 = System.currentTimeMillis();
 		tR = maxTime;
 		Coordinates curBestcoor = null;
 		int i = 1;
-		try{
-			while(i < maxDepth){
+		try {
+			while (i < maxDepth) {
 				i++;
-				System.out.println("Current Depth at:"+i);
-				//Now get best move
-				//System.out.println("Current depth:" + String.valueOf(i));
-				//System.out.println("Time used:" + String.valueOf(System.currentTimeMillis() - t0));
+				System.out.println("Current Depth at:" + i);
+				// Now get best move
+				// System.out.println("Current depth:" + String.valueOf(i));
+				// System.out.println("Time used:" +
+				// String.valueOf(System.currentTimeMillis() - t0));
 				curBestcoor = bestMove(gb.clone(), i);
 			}
-		}
-		catch(OutOfTimeException e){
-			System.out.println("Timeout! Last Depth at:"+i);
+		} catch (OutOfTimeException e) {
+			System.out.println("Timeout! Last Depth at:" + i);
+			allMoves.remove(curBestcoor);
 			return curBestcoor;
 		}
-		System.out.println("Last Depth at:"+ i);
+		System.out.println("Last Depth at:" + i);
+		allMoves.remove(curBestcoor);
 		return curBestcoor;
 	}
-	
-	private Coordinates bestMove(GameBoard gb, int maxDepth) throws OutOfTimeException{
+
+	private Coordinates bestMove(GameBoard gb, int maxDepth) throws OutOfTimeException {
 		CheckTimeOut();
-		
-		//Build and "solve" AlphaBeta Tree
-		//Get children
-		ArrayList<Coordinates> moves = new ArrayList<Coordinates>(calcAllValidMoves(gb, color));
-		
-		ArrayList<Integer> values = new ArrayList<Integer>();
+
+		// Build and "solve" AlphaBeta Tree
+		// Get children
+		// ArrayList<Coordinates> moves = new
+		// ArrayList<Coordinates>(calcAllValidMoves(gb, color));
+		// ArrayList<Integer> values = new ArrayList<Integer>();
+
 		int alpha = Integer.MIN_VALUE;
 		int beta = Integer.MAX_VALUE;
-		int bestIter = 0;
-		int bestVal = Integer.MIN_VALUE;
-		for(int i = 0; i < moves.size(); i++){
-			GameBoard nextgb = gb.clone();
-			nextgb.checkMove(color, moves.get(i));
-			nextgb.makeMove(color, moves.get(i));
-			values.add(minValue(nextgb, 1, maxDepth, alpha, beta));
-			if(values.get(i) >= bestVal){
-				bestVal = values.get(i);
-				bestIter = i;
-			}
+		int bestIter = Integer.MIN_VALUE;
+		Coordinates bestMove = null;
+		try {
+			StringBuilder moves = new StringBuilder();
+			for (Coordinates move : calcAllValidMoves(gb, color)) {
+
+				GameBoard nextgb = gb.clone();
+				nextgb.checkMove(color, move);
+				nextgb.makeMove(color, move);
+				// values.add(minValue(nextgb, 1, maxDepth, alpha, beta));
+				int currValue = minValue(nextgb, 1, maxDepth, alpha, beta);
+				
+				if (bestIter < currValue) {
+					bestIter = currValue;
+					bestMove = move;
+				}
+				moves.append("-" + currValue);
+			} 
+			System.out.println("Possible Moves: " + moves + " took " + bestIter) ;
+		} catch (NoMoveAvaiableException e) {
+			return null;
 		}
-		System.out.println("Last values " + values + " Took: " + bestVal);
-		//Get Coordinates with best value -> last child to set alpha
-		//System.out.println("Last alpha at:"+ alpha);
-		//System.out.println("Last bestVal at:"+ bestVal);
-		return moves.get(bestIter);
-	}
-	private int maxValue(GameBoard gb, int nextDepth, int maxDepth, int a, int b) throws OutOfTimeException{//Sets alpha
-		CheckTimeOut();
 		
-		if(nextDepth == maxDepth)
+		// Get Coordinates with best value -> last child to set alpha
+		// System.out.println("Last alpha at:"+ alpha);
+		// System.out.println("Last bestVal at:"+ bestVal);
+		return bestMove;
+	}
+
+	private int maxValue(GameBoard gb, int nextDepth, int maxDepth, int a, int b) throws OutOfTimeException {// Sets
+																												// alpha
+		CheckTimeOut();
+
+		if (nextDepth == maxDepth)
 			return pts(gb, color, other);
-		//Get all possible children
-		ArrayList<Coordinates> moves = new ArrayList<Coordinates>(calcAllValidMoves(gb, color));
-		if(moves.get(0) == null)
-			return pts(gb, color, other);
+		// Get all possible children
+		// ArrayList<Coordinates> moves = new
+		// ArrayList<Coordinates>(calcAllValidMoves(gb, color));
+		// if(moves.get(0) == null)
+		// return pts(gb, color, other);
 		int max = a;
-		//Go through children until alpha > beta or all children looked at
-		for(int i = 0; i < moves.size(); i++){
-			GameBoard nextgb = gb.clone();
-			nextgb.checkMove(color, moves.get(i));
-			nextgb.makeMove(color, moves.get(i));
-			int cur = minValue(nextgb, nextDepth+1, maxDepth, a, b);
-			//min = Integer.min(b, maxValue(nextgb, nextDepth+1, maxDepth, a, b));
-			//Now check if time to cut
-			if (cur > max){
-				max = cur;
-				if(max>=b)
-					break;
+		// Go through children until alpha > beta or all children looked at
+		try {
+			for (Coordinates move : calcAllValidMoves(gb, color)) {
+				GameBoard nextgb = gb.clone();
+				nextgb.checkMove(color, move);
+				nextgb.makeMove(color, move);
+
+				int cur = minValue(nextgb, nextDepth + 1, maxDepth, a, b);
+				// min = Integer.min(b, maxValue(nextgb, nextDepth+1, maxDepth,
+				// a, b));
+				// Now check if time to cut
+				if (cur > max) {
+					max = cur;
+					if (max >= b)
+						break;
+				}
 			}
+		} catch (NoMoveAvaiableException e) {
+			return pts(gb, color, other);
 		}
-		//return alpha
+		// return alpha
 		return max;
 	}
-	private int minValue(GameBoard gb, int nextDepth, int maxDepth, int a, int b) throws OutOfTimeException{//Sets beta
+
+	private int minValue(GameBoard gb, int nextDepth, int maxDepth, int a, int b) throws OutOfTimeException {// Sets
+																												// beta
 		CheckTimeOut();
-		
-		if(nextDepth == maxDepth)
+
+		if (nextDepth == maxDepth)
 			return pts(gb, color, other);
-		//Get all possible children
-		ArrayList<Coordinates> moves = new ArrayList<Coordinates>(calcAllValidMoves(gb, other));
-		if(moves.get(0) == null)
-			return pts(gb, color, other);
-		//Go through children until beta < alpha or all children looked at
+
+		// Go through children until beta < alpha or all children looked at
 		int min = b;
-		for(int i = 0; i < moves.size(); i++){
-			GameBoard nextgb = gb.clone();
-			nextgb.checkMove(other, moves.get(i));
-			nextgb.makeMove(other, moves.get(i));
-			int cur = maxValue(nextgb, nextDepth+1, maxDepth, a, b);
-			//min = Integer.min(b, maxValue(nextgb, nextDepth+1, maxDepth, a, b));
-			//Now check if time to cut
-			if (cur < min){
-				min = cur;
-				if(min<=a)
-					break;
+		try {
+			for (Coordinates move : calcAllValidMoves(gb, other)) {
+				GameBoard nextgb = gb.clone();
+				nextgb.checkMove(other, move);
+				nextgb.makeMove(other, move);
+				int cur = maxValue(nextgb, nextDepth + 1, maxDepth, a, b);
+				// min = Integer.min(b, maxValue(nextgb, nextDepth+1, maxDepth,
+				// a,
+				// b));
+				// Now check if time to cut
+				if (cur < min) {
+					min = cur;
+					if (min <= a)
+						break;
+				}
 			}
+		} catch (NoMoveAvaiableException e) {
+			return pts(gb, color, other);
 		}
-		//return beta
+		// return beta
 		return min;
 	}
-	private int pts(GameBoard GB, int us, int other){
+
+	private int pts(GameBoard GB, int us, int other) {
 		return PC.calcPoints(GB, us);
 	}
-	private ArrayList<Coordinates> calcAllValidMoves(GameBoard gb, int player){
-		ArrayList<Coordinates> res = new ArrayList<Coordinates>();
-		if(gb.isMoveAvailable(player)){
-			int row = 1, col = 1;
-			while(gb.validCoordinates(new Coordinates(row, col))){
-				while(gb.validCoordinates(new Coordinates(row, col))){
-					Coordinates curCoord = new Coordinates(row, col);
-					if(gb.checkMove(player, curCoord)){
-						GameBoard tempgb = gb.clone();
-						tempgb.checkMove(player, curCoord);
-						tempgb.makeMove(player, curCoord);
-						res.add(curCoord);
-					}
-					row++;
+
+	private ArrayList<Coordinates> calcAllValidMoves(GameBoard gb, int player) throws NoMoveAvaiableException {
+
+		ArrayList<Coordinates> res = new ArrayList<Coordinates>(10);
+
+		if (gb.isMoveAvailable(player)) {
+			for (Coordinates move: allMoves) {
+				if (gb.validCoordinates(move) && gb.checkMove(player, move)) {
+				//	System.out.println("Found valid move for \n " + gb + " move: " + move);
+					res.add(move);
 				}
-				row = 1;
-				col++;
 			}
-		}
-		else
-			res.add((Coordinates)null);
+		} else
+			throw new NoMoveAvaiableException();
+
 		return res;
 	}
-	private void CheckTimeOut() throws OutOfTimeException{
-		tR = maxTime - (System.currentTimeMillis() - t0);
-		if(tR <= 0)
+
+	private void CheckTimeOut() throws OutOfTimeException {
+		if (maxTime - (System.currentTimeMillis() - t0) <= 0)
 			throw new OutOfTimeException();
 	}
-	class OutOfTimeException extends Exception{}
+
+	class OutOfTimeException extends Exception {
+	}
+
+	class NoMoveAvaiableException extends Exception {
+	}
 }
